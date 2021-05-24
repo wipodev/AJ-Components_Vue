68e16148511612cd2b300cd12407e45aba01ba5b
cvcell <tropaguararia28@gmail.com>
2021-05-23 19:32:08 -0500

    configurando entorno

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h2>Custom Inputs!</h2>
+  <Baseinput type="text" name="user">User</Baseinput>
+  <Baseinput type="password" name="pass">password</Baseinput>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Baseinput from "./components/Baseinput.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Baseinput,
   },
 };
 </script>
